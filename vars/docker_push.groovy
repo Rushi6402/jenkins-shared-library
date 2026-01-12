76bfc16e2b5745e0b83b71bd5f5114eba6a3ddb5
Rushikesh Patil <rushikeshpatil6402@gmail.com>
2026-01-12 19:43:45 +0530

    Fix docker push command to use correct username

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -5,5 +5,5 @@ def call ( String Project , String ImageTag, String dockerHubUser){
                     passwordVariable:"dockerHubPass")]){
   sh ' docker login -u ${dockerHubUser} -p ${dockerHubPass} ' 
   }
-  sh '  docker push ${dockerUser}/${Project}:${ImageTag} '
+  sh '  docker push ${dockerHubUser}/${Project}:${ImageTag} '
 }
