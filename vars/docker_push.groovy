f25a0e31042873070290094ca2804052ea3a6af3
Rushikesh Patil <rushikeshpatil6402@gmail.com>
2026-01-12 19:46:44 +0530

    Fix string quotes in docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,7 +3,7 @@ def call ( String Project , String ImageTag, String dockerHubUser){
                     credentialsId:"dockerhub-credencial",
                     usernameVariable:"dockerHubUser", 
                     passwordVariable:"dockerHubPass")]){
-  sh ' docker login -u ${dockerHubUser} -p ${dockerHubPass} ' 
+  sh """ docker login -u ${dockerHubUser} -p ${dockerHubPass} """
   }
-  sh '  docker push ${dockerHubUser}/${Project}:${ImageTag} '
+  sh """ docker push ${dockerHubUser}/${Project}:${ImageTag} """
 }
